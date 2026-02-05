0c2ab9fdb919978cbe40d6df1d40e2fb9937f6ff
Angelo Silva <amsilvac1@uce.edu.ec>
2026-02-05 18:43:43 -0500

    Taller 36: Guardianes

--- src/router/index.js
+++ src/router/index.js
@@ -6,38 +6,61 @@ const router = createRouter({
     {
       path: '/',
       redirect: '/consultar-todos',
+      meta: { requiresAuth: false, esPublica: false },
     },
     {
       path: '/consultar-todos',
       name: 'consultarTodos',
       component: () => import('../components/ConsultarTodosComponent.vue'),
+      meta: { requiresAuth: false, esPublica: false },
     },
     {
       path: '/consultar-por-id',
       name: 'consultarPorId',
       component: () => import('../components/ConsultarPorIdComponent.vue'),
+      meta: { requiresAuth: false, esPublica: false },
     },
     {
       path: '/guardar',
       name: 'guardar',
       component: () => import('../components/GuardarComponent.vue'),
+      meta: { requiresAuth: true, esPublica: false },
     },
     {
       path: '/actualizar',
       name: 'actualizar',
       component: () => import('../components/ActualizarComponent.vue'),
+      meta: { requiresAuth: true, esPublica: false },
     },
     {
       path: '/actualizar-parcial',
       name: 'actualizarParcial',
       component: () => import('../components/ActualizarParcialComponent.vue'),
+      meta: { requiresAuth: true, esPublica: false },
     },
     {
       path: '/eliminar',
       name: 'eliminar',
       component: () => import('../components/EliminarComponent.vue'),
+      meta: { requiresAuth: true, esPublica: false },
     },
   ],
+})
+
+/*Configuración de guardianes de rutas
+  to = ruta a la que se quiere navegar
+   from = ruta desde la que se navega
+   next = función que permite continuar o cancelar la navegación
+*/
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    // le envio a una pagina de login o algo similar
+    console.log('Redirije al login')
+  } else {
+    //le dejo sin validación
+    console.log('Pase libre')
+    next()
+  }
 })
 
 export default router
