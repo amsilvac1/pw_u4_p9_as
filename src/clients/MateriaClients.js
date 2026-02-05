06192168c0218b54682d94f8b30af78938368840
Angelo Silva <amsilvac1@uce.edu.ec>
2026-02-04 22:32:26 -0500

    Taller 35: OAuth 2.0

--- src/clients/MateriaClients.js
+++ src/clients/MateriaClients.js
@@ -1,47 +1,63 @@
 import axios from 'axios'
 
+const TOKEN =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJtYXRyaWN1bGEtYXV0aCIsInN1YiI6ImFkbWluIiwiZ3JvdXBzIjpbImFkbWluIl0sImlhdCI6MTc3MDI0ODQwNCwiZXhwIjoxNzcwMjUyMDA0LCJqdGkiOiI4N2M3YjdiOS1mNzI4LTQyMjgtOGQwYi1hOGI1YWNjNTcxZmEifQ.PAVkvWZYQm-LBl89FRCUFVIh-Xz2weapxV1UOOzt8bavJzR3Mjza0Ds1IEMt06QmtHoXa2Kz9CAJpwyKNMCkeUHyjy0iQ8biO6g_vWSZY5k9QXL5vvd1-tHfWwbwPS0GKC336rBxqzEUMC8OtVj3MWRWI44Gsm6i_w-2U55X6ui9wgtkopvQ_XuIE_vkDFnLUtgPhXBJtWFImli1HSJO6Sf-fKJflTfsULnXqiDSMtpuiVIc4P0NjZSWoeh25aUD6wQUQ6Y-S39rZfEv-ddpzwUt4vwpVev9O-ZxiQSJ8ymCL273V3ExeLu0CKPokDJSEP9xp7IoVLUKroVDOaWcfQ'
+
 const consultarTodos = () => {
   const data = axios
-    .get('http://localhost:8081/matricula/api/v1.0/estudiantes')
+    .get('http://localhost:8081/matricula/api/v1.0/estudiantes', {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
   return data
 }
 
 const consultarPorId = (id) => {
   const data = axios
-    .get(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`)
+    .get(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
   return data
 }
 
 const guardar = (body) => {
+  //data es lo que recibe desde el body
   /* const objeto = {
         nombre:Angelo,
         apellido: Silva
   } */
   const data = axios
-    .post('http://localhost:8081/matricula/api/v1.0/estudiantes', body)
+    .post('http://localhost:8081/matricula/api/v1.0/estudiantes', body, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
   return data
 }
 
 const actualizar = (id, body) => {
   const data = axios
-    .put(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, body)
+    .put(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, body, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
   return data
 }
 
 const actualizarParcial = (id, body) => {
   const data = axios
-    .patch(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, body)
+    .patch(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, body, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
   return data
 }
 
 const eliminar = (id) => {
   axios
-    .delete(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`)
+    .delete(`http://localhost:8081/matricula/api/v1.0/estudiantes/${id}`, {
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
     .then((r) => r.data)
 }
 
